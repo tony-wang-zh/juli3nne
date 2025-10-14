a4d1dde03394ff84403a2a97a3deb2e8e070b0fd
TonyWang <zw3144@columbia.edu>
2025-10-13 20:43:22 -0400

    python dependency that works on ubuntu 18

--- server.py
+++ server.py
@@ -92,6 +92,6 @@ def upload():
 if __name__ == '__main__':
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config['STATIC_STL_FOLDER'] = STATIC_STL_FOLDER
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
 
 
